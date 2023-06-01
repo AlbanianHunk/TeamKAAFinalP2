841b3415a30a448982c10967a3b4d34ee5561557
AlbanianHunk <116110355+AlbanianHunk@users.noreply.github.com>
2023-05-31 22:43:56 -0700

    Revert "Game Manager"
    This reverts commit 621f0cba3306029b96cd279e816af11f94ef7af5.

--- TeamKAAFinalAksabanovicP2/Assets/Scripts/GoalCounter.cs
+++ TeamKAAFinalAksabanovicP2/Assets/Scripts/GoalCounter.cs
@@ -4,6 +4,32 @@ using UnityEngine;
 
 public class GoalCounter : MonoBehaviour
 {
+    public int scoreValue;
+    // reference to playerstats
+    public PlayerStats playerStats;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //  get playerstats fom wich ever object has it
+        playerStats = GameObject.FindObjectOfType(typeof(PlayerStats)) as PlayerStats;
+        playerStats.UpdateStats();
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // get reference to collider (to find the trigger one)
+        Collider m_col = gameObject.GetComponent<Collider>();
+
+        if (other.name != "Player" && m_col.isTrigger)
+        {
+            playerStats.AddScore(+1);
+            Destroy(gameObject);
+        }
+    }
 }
-    
--- TeamKAAFinalAksabanovicP2/Assets/Scripts/Kick.cs
+++ TeamKAAFinalAksabanovicP2/Assets/Scripts/Kick.cs
@@ -6,8 +6,6 @@ public class Kick : MonoBehaviour
 {
     private Rigidbody rb;
 
-
-
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
@@ -18,5 +16,11 @@ public class Kick : MonoBehaviour
         rb.AddForce(-transform.forward * 750f);
         rb.useGravity = true;
     }
-
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("You lose");
+        }
+    }
 }
